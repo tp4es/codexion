1757f737f1035f97cb85de131be9cada01e75852
Tiago_laptop <tide-oli@student.42madrid.com>
2026-09-06 13:42:17 +0200

    started

--- main.c
+++ main.c
@@ -6,14 +6,15 @@
 /*   By: tide.oli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/06 13:01:37 by tide.oli          #+#    #+#             */
-/*   Updated: 2026/09/06 13:33:32 by tide.oli         ###   ########.fr       */
+/*   Updated: 2026/09/06 13:42:08 by tide.oli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
-char	*setup(char **input)
+char	*setup(int len, char **input, t_config load)
 {
+	
 	if ()
 }
 
@@ -22,5 +23,5 @@ int	main(int argc, char **argv)
 	t_config	load;
 	if (argc != 8)
 		return (1);
-	
+	setup((argc - 1), (argv + 1), load);
 }
